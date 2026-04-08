9161c8d5d0944f126f857ed31f5bda7d643e626f
Aniruddh <anilkharabe@gmail.com>
2026-04-08 08:54:25 +0530

    updated cors

---
+++ readme.md
@@ -0,0 +1,9 @@
+1. Pull the code.
+2. Install dependecies using npm i
+3. start server: node server.js
+
+4. use this URL to make request
+    http://localhost:5000/api/menu?restaurantId=16061
+
+go to localhost:1234
+ctrl + shift + r => hard refresh => delete cache/cookies
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const menuRoute = require("./routes/menu");
+const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
+// res.setHeader('Access-Control-Allow-Origin', '*');
 
 app.use("/api/menu", menuRoute);
 
